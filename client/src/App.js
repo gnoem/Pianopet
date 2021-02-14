72b4ae9bf2349e618ffdb07b44d1941cd6e82dc3
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-02-14 01:24:04 -0800

    converted app.js to functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,44 @@
 import './App.css';
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Loading from './components/Loading';
 import Guest from './components/Guest';
 import Student from './components/Student';
 import Teacher from './components/Teacher';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.logout = this.logout.bind(this);
-        this.getData = this.getData.bind(this);
-        this.state = {
-            isLoaded: false
-        }
-    }
-    componentDidMount() {
-        this.getData();
-    }
-    getData = async () => {
+export default function App() {
+    const [student, setStudent] = useState(false);
+    const [teacher, setTeacher] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        getData();
+    }, []);
+    const getData = async () => {
         const response = await fetch('/auth');
         const body = await response.json();
-        if (!body) return console.log('server error');
-        if (!body.success) return this.setState({ isLoaded: true, student: false, teacher: false });
-        if (body.student) return this.setState({ isLoaded: true, student: body.student });
-        if (body.teacher) return this.setState({ isLoaded: true, teacher: body.teacher });
+        if (!body) return console.log('no response from server');
+        if (!body.success) return setIsLoaded(true);
+        if (body.student) {
+            setStudent(body.student);
+            setIsLoaded(true);
+            return;
+        }
+        if (body.teacher) {
+            setTeacher(body.teacher);
+            setIsLoaded(true);
+            return;
+        }
     }
-    logout = async () => {
+    const logout = async () => {
         fetch('/logout').then(() => window.location.assign('/'));
     }
-    render() {
-        const { isLoaded, student, teacher } = this.state;
-        const app = () => {
-            if (!student && !teacher) return <Guest />;
-            if (student) return <Student logout={this.logout} />;
-            if (teacher) return <Teacher teacher={teacher} refreshTeacher={this.getData} logout={this.logout} />;
-        }
-        console.log('app loaded');
-        return (
-            <div className="App">
-                {isLoaded ? app() : <Loading />}
-            </div>
-        )
+    const app = () => {
+        if (!student && !teacher) return <Guest />;
+        if (student) return <Student logout={logout} />;
+        if (teacher) return <Teacher teacher={teacher} refreshTeacher={getData} logout={logout} />;
     }
+    return (
+        <div className="App">
+            {isLoaded ? app() : <Loading />}
+        </div>
+    )
 }
-
-export default App;
