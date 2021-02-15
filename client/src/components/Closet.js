21d86b71a025e81bf42ef2714815618f33857ff8
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-02-15 00:24:42 -0800

    forgot to add new files before committing

---
+++ client/src/components/Closet.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+
+export default function Closet(props) {
+    const { closet, teacher } = props;
+    const [category, setCategory] = useState(() => {
+        return teacher.wearableCategories.find(category => closet.some(wearable => wearable.category === category));
+    });
+    const generate = {
+        categoriesList: (closet) => {
+            return teacher.wearableCategories.map(category => {
+                const someClosetItemHasCategory = closet.some(wearable => wearable.category === category);
+                if (someClosetItemHasCategory) return (
+                    <button
+                      key={`closet-wearableCategories-${category}`}
+                      className="stealth"
+                      onClick={() => setCategory(category)}>
+                          {category}
+                    </button>
+                )
+            });
+        },
+        wearablesList: (category) => {
+            const previewWearable = ({ category, _id, src }) => {
+                props.updateAvatar(prevState => {
+                    if (prevState[category]._id === _id) {
+                        let prevStateMinusThisCategory = {...prevState};
+                        delete prevStateMinusThisCategory[category];
+                        return prevStateMinusThisCategory;
+                    }
+                    return ({
+                        ...prevState,
+                        [category]: { _id, src }
+                    });
+                });
+            }
+            return closet.map(wearable => {
+                if (wearable.category === category) return (
+                    <button
+                      key={`closetItem-${category}-${wearable._id}`}
+                      className={`stealth closetItem`} // if currently previewing, add light green background or something
+                      onClick={() => previewWearable(wearable)}>
+                        <img alt={wearable.name} src={wearable.src} />
+                        <span className="wearableName">{wearable.name}</span>
+                    </button>
+                );
+            });
+        }
+    }
+    return (
+        <div className="Closet">
+            <div id="demo" onClick={() => console.table(props.avatar)}></div>
+            <div className="closetCategories">
+                {generate.categoriesList(closet)}
+            </div>
+            <div className="closetWearables">
+                <div className="wearablesGrid">
+                    {generate.wearablesList(category)}
+                </div>
+            </div>
+        </div>
+    );
+}
