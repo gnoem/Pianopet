0c4aa7f63353eea276857479fb251d01ecb8335f
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-02-23 01:43:33 -0800

    mongo auth

--- server/app/config/db.js
+++ server/app/config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const { MONGO_URI } = require('./');
+const { MONGO_AUTH } = require('./');
 
-const uri = process.env.MONGO_URI || MONGO_URI;
+const auth = process.env.MONGO_AUTH || MONGO_AUTH;
 
 module.exports = async () => {
-    mongoose.connect(uri, {
+    mongoose.connect(`mongodb+srv://admin:${auth}@cluster0.nxvhj.mongodb.net/pianopet?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false
