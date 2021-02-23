560a40e25518c864304c136632e066dd4ae1b63c
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-02-22 21:51:20 -0800

    prod test

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname + '../client/build/index.html'));
+    });
+}
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,7 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 require('./app/config/db')();
 require('./app/routes')(app);
 
-let PORT = 6000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}`);
+    console.log(process.env.NODE_ENV);
 });
