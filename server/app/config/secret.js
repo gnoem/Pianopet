d13cde2e8394af1133ac727ed4cdc1af2f51c04e
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-04-07 14:24:19 -0700

    banished DS_Store

--- server/app/config/secret.js
+++
@@ -1,8 +0,0 @@
-const secretKey = async () => {
-    const vars = (await import('./vars.js')).default;
-    if(process.env.NODE_ENV === 'production')
-        return process.env.SECRET_KEY;
-    return vars.SECRET_KEY;
-}
-
-export default (await secretKey());
